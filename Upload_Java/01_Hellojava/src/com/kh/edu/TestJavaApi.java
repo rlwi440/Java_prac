e6865bac788610d9759225dfadc5c8e1883e648d
rlwi440 <rlwi440@naver.com>
2020-12-07 07:17:37 +0900

    [add]practice Upload

--- Upload_Java/01_Hellojava/src/com/kh/edu/TestJavaApi.java
+++ Upload_Java/01_Hellojava/src/com/kh/edu/TestJavaApi.java
@@ -3,7 +3,15 @@ package com.kh.edu;
 import java.util.Date;
 
 public class TestJavaApi {
+	/**
+	 * @author 깔끔한청년
+	 * @date 2020.12.07
+	 */
 	public static void main(String[] args) {
+		/**
+		 * @param Test1,Test2
+		 * 
+		 */
 		TestJavaApi abc= new TestJavaApi();
 		abc.Test1();
 		abc.Test2();
