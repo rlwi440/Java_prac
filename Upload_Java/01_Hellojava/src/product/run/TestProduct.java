42e46d2b9f1e72b74ddec80a7d0761ab80def086
rlwi440 <rlwi440@naver.com>
2020-12-07 07:35:59 +0900

    [add] class child object(Product)creation

--- Upload_Java/01_Hellojava/src/product/run/TestProduct.java
+++ Upload_Java/01_Hellojava/src/product/run/TestProduct.java
@@ -4,11 +4,5 @@ public class TestProduct {
 	
 	public static void main(String[] args) {
 		
-		TestProduct abc = new TestProduct();
-		abc.Test1();
 	}
-	public void Test1() {
-		System.out.println("성공여부");
-	}
-
-}
+	
