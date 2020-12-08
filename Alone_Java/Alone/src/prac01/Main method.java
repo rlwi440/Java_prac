191dc087741e0966c3e2b2d5a5fef990714bef6a
rlwi440 <rlwi440@naver.com>
2020-12-09 08:28:51 +0900

    [add] main mehtod import practice

---
+++ Alone_Java/Alone/src/prac01/Main method.java
@@ -0,0 +1,16 @@
+package com.test.car.over;
+import com.test.car.carExample;		//import 연습
+
+
+public class Main {
+	public static void main(String[] args) {
+	 
+		carExample ki= new carExample();
+		
+		ki.test1();
+		ki.test2();
+		ki.test3();
+		
+	}
+
+}
---
+++ Alone_Java/Alone/src/prac01/castprac.java
@@ -0,0 +1,6 @@
+package parc02;
+
+public class Castprac {
+    public Static void main(String[]args){
+        Casting ex =new ca\\777\ 
+    }
