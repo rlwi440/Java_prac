c05a301c86f887d79d0ea00d427bba5c7d5c3b80
rlwi440 <rlwi440@naver.com>
2020-12-10 07:51:25 +0900

    [update] Operator syntax Scanner method

--- Upload_Java/01_Hellojava/src/Test/condition/Conditionla method practice alone.java
+++ Upload_Java/01_Hellojava/src/Test/condition/Conditionla method practice alone.java
@@ -3,37 +3,45 @@ package kh.java.condition.Test3;
 import java.util.Scanner;
 
 public class Test3 {
-    /**
-     * @author 깔끔한 청년
-     * @date 2020.12.10
-     */
+	/**
+	 * @author 깔끔한청년
+	 * @date 2020.12.10
+	 */
 	public static void main (String[]args) {
+		/**
+		 * @param num1,num2,ch
+		 * @thtrow Scanner 매서드
+		 */
 		
 		Test3 ki =new Test3 ();
 		ki.test3();
 	}
 	public void test3() {
-		Scanner sc= new Scanner(System.in);
-			System.out.println("정수를 입력하세요.");
-			int num1 =sc.nextInt();
+		int num1;	// 첫번째 연산자
+		int num2;
+		char ch;	//연산자 
 		
-			System.out.println("정수를 입력하세요.");
-			int num2 =sc.nextInt();
-			 
-			System.out.println("연산자를 입력");
-			char ch =sc.next().charAt(0);
-	
-	if (ch== '+') {
-		System.out.println(num1+  "+"+num2 +"="  +(num1+num2));
-	}
-	else if(ch == '-') {
-		System.out.println(num1+  "-"+num2 +"="  +(num1-num2));
-	}
-	else if(ch == '*') {
-		System.out.println(num1+  "*"+num2 +"="  +(num1*num2));
-	}
-	else {
-		System.out.println("잘못입력하셨습니다.프로그렘 종료");
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.printf("오른쪽 정수 입력구간: ");
+		num1=sc.nextInt();
+		System.out.printf("왼쪽 정수 입력구간:");
+		num2=sc.nextInt();
+		System.out.printf("연산자 입력");
+		ch= sc.next().charAt(0);		// char 형식은  sc.next.charAt(0)
+		
+		if(ch=='+') {
+			System.out.println(num1+ "+" +num2 +"=" +(num1+num2));
+		}else if(ch== '-') {
+		System.out.println(num1+ "-" +num2 +"=" +(num1-num2));
+			}else if(ch=='*') {
+				System.out.println(num1+ "*" +num2 +"=" +(num1*num2));
+		}else if(ch=='/') {
+			System.out.println(num1+ "/" +num2 +"=" +(num1/num2));
+		}else {
+			System.out.println("잘못 입력을 하셨습니다. 프로그램 종료 합니다.");
+		
 		}
+		
 	}
 }
