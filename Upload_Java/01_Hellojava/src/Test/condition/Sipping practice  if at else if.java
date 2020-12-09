4f6d8aa7204d5c17bd0c55a53a598eabb0cda47b
rlwi440 <rlwi440@naver.com>
2020-12-10 07:31:13 +0900

    [update] odd even number program  alone practice

--- Upload_Java/01_Hellojava/src/Test/condition/Sipping practice  if at else if.java
+++ Upload_Java/01_Hellojava/src/Test/condition/Sipping practice  if at else if.java
@@ -3,29 +3,32 @@ package kh.java.condition.Test3;
 import java.util.Scanner;
 
 public class Test4 {
-    /**
-    * @author 깔끔한청년
-    * @date  2020.12.10
-    */
+	/**
+	 * @author 깔끔한청년
+	 * @date 2020.12.10
+	 * 
+	 */
 
 	public static void main(String[] args) {
+		/**
+		 * @param 
+		 */
 		Test4 ki =new Test4();
 		ki.Test();
 
 	}
 	public void Test() {
+		int num;
 		Scanner sc= new Scanner(System.in);
 		System.out.println("정수입력 :");
-	 int num =sc.nextInt();	
-	
-	 if(num>0 &&num <=10) {
-		 if(num%2==0) {
-			 System.out.println("짝수");
-		 } else   {
-			 System.out.println("홀수");
-		 } else  {
-			 System.out.println("1~10사이 정수 를 입력");
-		 }
-	 }
+		num= sc.nextInt();
+		if(0>num&&10<=num) {
+		}else if(num%2==0) {
+			System.out.printf("짝수인지 확인",num);
+			}else if(num%2!=0) {
+				System.out.printf("홀수인지 확인",num);
+			} else {
+				System.out.println("반드시 정수 1~10사이의 정수입력해야합니다");	
+			}
 	}
 }
