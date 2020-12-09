74547621391128f0a13f86c5f91a9e7e9aec6a1a
rlwi440 <rlwi440@naver.com>
2020-12-10 07:08:10 +0900

    [update] BMI Double alone  practice

--- Upload_Java/01_Hellojava/src/Test/condition/bmiprogrampractice.java
+++ Upload_Java/01_Hellojava/src/Test/condition/bmiprogrampractice.java
@@ -3,40 +3,45 @@ package kh.java.condition.Test3;
 import java.util.Scanner;
 
 public class Test5 {
-    /**
-     * @author 깔끔한청년
-     * @date 2020.12.10
-     */
 	public static void main (String[]args) {
+		/**
+		 * @author 깔끔한청년
+		 * @date 2020.12.10 
+		 */
 		Test5 ki= new Test5();
-		ki.Test();
-	}
-	
-	public void Test() {
-		Scanner sc=new Scanner(System.in);
+		/**
+		 * @parma double
+		 * @throws BMI calc
+		 */
+		ki.test();
 		
-		System.out.println("키(cm)을 입력");
-		int cm =sc.nextInt();
+		}
+	public void test() {
+		double weight;
+		double height;
+		double BMI;
 		
-		System.out.println("몸무게(kg)을 입력");
-		double kg =sc.nextDouble()
+		Scanner sc= new Scanner(System.in);
 		
+		System.out.println("키 입력 구간입니다. :");
+		 height=sc.nextDouble();
+		 
+		 System.out.println("몸무게 입력 구간입니다. :");
+		 weight=sc.nextDouble();
+		 
+	 	BMI = weight*1000/(height*height);		//BMI = 몸무게*10000/(신장(M)*신장(M))
 		
-		double bmi= kg/((cm/100.0)*(cm/100.0));	
-	}
-	if(bmi>=30) {
-		System.out.println("고도비만");
-	}else if(bmi>= 25&& bmi <30 ) {
-		System.out.println("비만");
-	}else if(bmi>= 23&& bmi <25 ) {
-		System.out.println("과체중");
-	}else if(bmi>= 18.5&& bmi <23 ) {
-		System.out.println("정상체중");{
-		else {
-			System.out.println("저체중");  
-			}
+		if(BMI>=30) {
+			System.out.printf("체지량 지수%.1f상 고도비만 입니다",BMI);
+		}else if(BMI>=25&&30<BMI) {
+			System.out.printf("체질량 지수 %.1f상 비만입니다",BMI);
+		}else if(BMI>=23&&25<BMI) {
+			System.out.printf("체질량 지수 %.1f상 과체중 입니다",BMI);
+		}else if(BMI>=18.5&&23<BMI) {
+			System.out.printf("체질량 지수 %.1f상 정상체중입니다",BMI);
+		}else {
+			System.out.printf("체질량 지수 %.1f상 저체중입니다",BMI);
 		}
 	}
-
-		
-
+}
+	
