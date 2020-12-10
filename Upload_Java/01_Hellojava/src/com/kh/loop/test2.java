b13262414e90e1f459df2e959e962f3ad0678566
rlwi440 <rlwi440@naver.com>
2020-12-11 08:19:45 +0900

    [update] Integer input for sectence practice

--- Upload_Java/01_Hellojava/src/com/kh/loop/test2.java
+++ Upload_Java/01_Hellojava/src/com/kh/loop/test2.java
@@ -3,13 +3,17 @@ package com.kh.test.loop;
 import java.util.Scanner;
 
 public class Test2 {
-
+	/**
+	 * @author 깔끔한청년
+	 * @date 2020.12.11
+	 * 
+	 */
 	public static void main(String[] args) {
 		int num;
 		Scanner sc= new Scanner(System.in);
 		System.out.println("정수를 입력해주세요");
 		num=sc.nextInt();
-		for(i=1;i<num;i++) {
+		for(int i=1; i<num; i++) {			//int 수정
 			if(i%2==0) {
 				System.out.println("박");
 			}else {
