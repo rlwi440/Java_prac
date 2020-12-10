00a4806185377ffe663c7377e810f2fce5689ac0
rlwi440 <rlwi440@naver.com>
2020-12-11 08:39:01 +0900

    [update] Multiplication table 2~9 output

--- Upload_Java/01_Hellojava/src/com/kh/loop/test3.java
+++ Upload_Java/01_Hellojava/src/com/kh/loop/test3.java
@@ -1,24 +1,25 @@
 package com.kh.test.loop;
 
-import java.util.Scanner;
-
 public class Test3 {
+	/**
+	 * @author 깔끔한청년
+	 * @Date 2020.12.11  
+	 */
 	public static void main(String[]args) {
 		Test3 ki=new Test3();
 		ki.test();
 	}
 	public void test() {
-		int dan ; 
-		Scanner sc= new Scanner(System.in);
-		System.out.println("정수를 입력하세요 : ");
-		if(dan>=1 && dan<= 9) {
-			for(int i=1;i<9;i++) {
-				System.out.printf("%dx%d=%d",dan i dan*i);
+
+		for(int i=1;i<10;i++) {
+			System.out.println(i+"단을 출력합니다" );
+			for(int j=1;j<10;j++) {
+				System.out.println(i+"x"+j+"="+(i*j));
 			}
-		}
-		else {
+		 {
 			System.out.println("잘못입력하셨습니다. 프로그렘종료");
 		}
 	}
 
+	}
 }
