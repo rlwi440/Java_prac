1dc6951e32c8aa3745b190332cfa87cebb653819
rlwi440 <rlwi440@naver.com>
2020-12-15 08:25:02 +0900

    [add] string [] method

---
+++ Upload_Java/src/kh/java/test/array/Test2.java
@@ -0,0 +1,19 @@
+package kh.java.test.array;
+
+public class Test2 {
+	public static void main(String[]args) {
+		Test2 ki =new Test2();
+		ki.test();
+	}
+	public void test() {
+		/**
+		 * string[] name  배열을 선언후   배열 인덱스로 바나나 출력 
+		 * @author 깔끔한 청년
+		 * @Date 2020.12.14
+		 * 
+		 */
+		String [] name ={"딸기","복숭아","키위","사과","바나나"};
+		System.out.printf(name[4]);
+		}
+	}
+
