88449901f9ae390bd0976ba690d0846fd9b5989f
rlwi440 <rlwi440@naver.com>
2020-12-15 08:07:42 +0900

    [add] Output in order of arrangement

---
+++ Upload_Java/src/kh/java/test/array/Test1.java
@@ -0,0 +1,26 @@
+package kh.java.test.array;
+
+public class Test1 {
+		/**
+		 * 길이가 100인 배열을 선언하고 1부터 100까지 의값 을 순서대로 배열 
+		 * @author 깔끔한청년
+		 * @Date 2020.12.14 
+		 * 
+		 */
+	
+
+	public static void main(String[] args) {
+			Test1 ki =new Test1();
+			ki.test();
+	}
+	public void test() {
+			int[]arr =new int[100];		//길이값 100 
+			int sum=0;		 //저장 공간
+			for(int i=1; i<arr.length; i++) {
+				sum +=i;
+				System.out.println(sum);
+		}
+		
+	}
+
+}
