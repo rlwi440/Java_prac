9f87d037eb7acd2fc99e17ea513c7212456856be
rlwi440 <rlwi440@naver.com>
2020-12-20 13:04:35 +0900

    [add] String mehod  practice

---
+++ Alone_Java/Alone/src/OPPprac/Static_RUN.java
@@ -0,0 +1,13 @@
+package kh.java.oop.method.static_;
+
+public class Static_RUN {
+	public static void main(String[] args) {
+		StaticMethod ki= new StaticMethod();
+		System.out.println(ki.toUpper("studynote"));
+		System.out.println(ki.setChar(" ", 0));
+		System.out.println(ki.getAlphabetLength("abcdefg"));
+		System.out.println(ki.conact("no", null));
+		}
+	}
+
+
