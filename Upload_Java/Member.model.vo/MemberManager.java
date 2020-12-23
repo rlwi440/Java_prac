12f22470a244cd68fddb21ffa0f9894368be0e80
rlwi440 <rlwi440@naver.com>
2020-12-24 08:20:28 +0900

    [update]  Vip,VVip child class add

--- Upload_Java/Member.model.vo/MemberManager.java
+++ Upload_Java/Member.model.vo/MemberManager.java
@@ -3,7 +3,7 @@ package Member.model.vo;
 //giwon.point.Slaver
 //giwon.point.Gold
 public class MemberManager {
-	
+
 	   String member, grade;
 	   double point;
 
@@ -13,16 +13,31 @@ public class MemberManager {
 	   Gold[] gold = new Gold[10];
 	   int goldInt = 0;
 	   
+	   Vip [] vip =new Vip[10];
+	   int vipInt = 0;
+	   
+	   VVip [] vvip =new VVip[10];
+	   int VVipInt = 0;
+	   
 	   public void silverInsert(Slaver slaver) {
 			
 		   this.slaver[this.slaverInt] = slaver;
 		   this.slaverInt += 1;
-		   
 	   }
 		
 		public void goldInsert(Gold gold) {
 			this.gold[this.goldInt] = gold;
 			this.goldInt += 1;
+		}
+		
+		public void vipInsert(Vip vip) {
+			this.vip[this.vipInt] = vip;
+			this.vipInt += 1;
+		}
+		
+		public void VVipInsert(VVip vvip) {
+			this.vvip[this.vipInt] = vvip;
+			this.VVipInt += 1;
 		}
 		
 		public void printData() {
@@ -40,5 +55,18 @@ public class MemberManager {
 				System.out.println(this.gold[i].point);
 				System.out.println(this.gold[i].countPoint);
 			}
+			
+			for(int i=0; i<this.vipInt; i++) {
+				System.out.println(this.vip[i].member);
+				System.out.println(this.vip[i].grade);
+				System.out.println(this.vip[i].point);
+				System.out.println(this.vip[i].countPoint);
+			}
+			for(int i=0; i<this.VVipInt; i++) {
+				System.out.println(this.vvip[i].member);
+				System.out.println(this.vvip[i].grade);
+				System.out.println(this.vvip[i].point);
+				System.out.println(this.vvip[i].countPoint);
+			}
 		}
-}
+	}
