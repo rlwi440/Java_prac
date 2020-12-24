82eba773b112c787c42236d056f3ae2fc8361172
rlwi440 <rlwi440@naver.com>
2020-12-24 11:54:46 +0900

    [add] practice for finding radius child

---
+++ Alone_Java/Alone/src/UMLprac/Circle.java
@@ -0,0 +1,23 @@
+package com.kh.test.shpe.model.vo;
+
+public class Circle {
+	public static final double PI= Math.PI;
+	private int radius;
+	
+	public Circle () {		
+	}
+	public Circle(int radius) {
+		this.radius=radius;
+		
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	public void draw() {
+		System.out.println("반지름"+radius+"cm인 원을 그립니다.");
+		
+	}
+}
