8e2150212beacd044704a326475e0f170b51f19d
rlwi440 <rlwi440@naver.com>
2020-12-27 00:18:02 +0900

    [add] Class with TIre parts practice alone

---
+++ Alone_Java/Alone/src/Polymorhhism/Car.java
@@ -0,0 +1,22 @@
+package com.ki.test.field;
+
+public class HankookTire extends Tire {
+
+	public HankookTire(String location, int maxRotation) {
+		super(location, maxRotation);
+	}
+	@Override
+	
+	public boolean roll() {
+		 ++accumulationRotation;	//누적 회전수 1증가 	
+		 if(accumulationRotation<maxRotation) {
+			 System.out.println(location + "HaNkookTire 수명 : "+(maxRotation-accumulationRotation)+"회");		//정상회전(최대) 일 경우 실행
+			 return true;
+		 }else {
+			 System.out.println("***"+location+"HanKookTire펑크 ***");	//펑크(누적=최대)일경우 실행
+			 return false;
+		
+		 	}	
+		}
+	}
+s
